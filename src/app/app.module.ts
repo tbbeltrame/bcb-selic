7094466c4f20a1a90799a102996f9242606ed62a
thalya.beltrame <thalya.beltrame@radixeng.com.br>
2021-05-23 22:24:45 -0300

    v01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { SelicService } from './selic.service';
+import { DatePickerModule } from './date-picker/date-picker.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    DatePickerModule
   ],
-  providers: [],
+  providers: [
+    SelicService,
+    {
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
