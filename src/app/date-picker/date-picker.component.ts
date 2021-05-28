2c01055a530c7a2ac750b3fd5313fbecaf9c19ba
Lucas Felippe Mateus <lucasfelippemateus@yahoo.com.br>
2021-05-28 14:36:00 -0300

    Calling RequestSelic through ngOnChanges

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { TranslateService } from '@ngx-translate/core';
 @Component({
@@ -6,25 +6,19 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './date-picker.component.html',
   styleUrls: ['./date-picker.component.css'],
 })
-export class DatePickerComponent implements OnInit {
+export class DatePickerComponent {
   minDate = new Date(1986, 6, 4);
   maxDate = new Date();
-  startDate: Date;
-  endDate: Date;
-  ready: boolean = false;
+  startDate = new Date(1986, 6, 4);
+  endDate= new Date(1986, 6, 4);
 
   constructor(private translate: TranslateService) {}
 
-  ngOnInit() {}
-
   startDateChange(event: MatDatepickerInputEvent<Date>) {
     this.startDate = event.value;
-    this.ready = false;
   }
 
   endDateChange(event: MatDatepickerInputEvent<Date>) {
     this.endDate = event.value;
-    this.ready = true;
-    
   }
 }
--- src/app/selic/selic.component.ts
+++ src/app/selic/selic.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges} from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, SimpleChanges} from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Selic, SelicService } from '../selic/selic.service';
 @Component({
@@ -6,33 +6,47 @@ import { Selic, SelicService } from '../selic/selic.service';
   templateUrl: './selic.component.html',
   styleUrls: ['./selic.component.css']
 })
-export class SelicComponent implements OnChanges{
+export class SelicComponent implements OnChanges, DoCheck{
 
   @Input() startDate = new Date();
   @Input() endDate = new Date();
-  @Input() ready: boolean;
+
+  /**@Input()
+  set a(a:string) {
+    this._a = a;
+    console.log('a updated');
+  } */
   selicData: Selic[] = [];
   hidden: boolean = true;
   hiddenLoader: boolean = false;
   
   constructor(private selicService: SelicService, private translate: TranslateService) { }
-  
-  ngOnChanges(){
-    if(this.ready){
-      if (this.startDate <= this.endDate) {
-        this.hidden = true;
-        this.hiddenLoader = true;
-        this.selicService.getSelic(this.startDate, this.endDate).subscribe((data) => {
-          this.selicData = data;
-          this.hiddenLoader = !data.length;
-          this.hidden = !data.length;
-        });
-      } else {
-        alert(this.translate.instant('warning.message-dates'));
-        this.hiddenLoader = false;
-        this.hidden = true;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if('endDate' in changes)
+    {
+      if(!changes.endDate.isFirstChange())
+      {
+        this.RequestSelic();
       }
     }
   }
 
+  ngDoCheck() {}
+
+  RequestSelic(){
+    if (this.startDate <= this.endDate) {
+      this.hidden = true;
+      this.hiddenLoader = true;
+      this.selicService.getSelic(this.startDate, this.endDate).subscribe((data) => {
+        this.selicData = data;
+        this.hiddenLoader = !data.length;
+        this.hidden = !data.length;
+      });
+    } else {
+      alert(this.translate.instant('warning.message-dates'));
+      this.hiddenLoader = false;
+      this.hidden = true;
+    }
+  }
 }
